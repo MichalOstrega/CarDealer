3c62c9a38ab7b401bac9ae86b58c4381429565b9
Michal_Ostrega <michalostrega@gmail.com>
2018-07-14 09:46:00 +0200

    Initial commit

---
+++ src/main/java/pl/sdacademy/cardealer/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package pl.sdacademy.cardealer;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class ApplicationConfiguration {
+}
