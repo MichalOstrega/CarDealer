38a0b3848d6195ebd295d2807ddd42c55e0e552f
Michal_Ostrega <michalostrega@gmail.com>
2018-07-28 11:12:03 +0200

    Beginning of Spring Security

--- src/main/java/pl/sdacademy/cardealer/ApplicationConfiguration.java
+++ src/main/java/pl/sdacademy/cardealer/ApplicationConfiguration.java
@@ -3,9 +3,47 @@ package pl.sdacademy.cardealer;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableAutoConfiguration
-public class ApplicationConfiguration {
+@EnableWebSecurity
+public class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication().
+                withUser("user")
+                .password("user")
+                .roles("user");
+        auth.inMemoryAuthentication().
+                withUser("admin")
+                .password("admin")
+                .roles("admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasRole("user")
+                .antMatchers("/accounts/**").hasRole("admin")
+                .and()
+                .formLogin()
+                .loginPage("/")
+                .loginProcessingUrl("/authenticateTheUser")
+                .successForwardUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and().exceptionHandling().accessDeniedPage("/access-denied");
+        http.csrf().disable();
+    }
 }
